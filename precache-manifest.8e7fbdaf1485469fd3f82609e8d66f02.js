37f65c2cc0c97c79084e93ff3b43af0d36f59a90
AlpacaGoesCrazy <vancho.karamba@gmail.com>
2019-11-30 19:36:16 +0200

    Updates

---
+++ precache-manifest.8e7fbdaf1485469fd3f82609e8d66f02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b7fe47709d585253f3a18b003dffa8",
+    "url": "/fact-seagull/index.html"
+  },
+  {
+    "revision": "5a898ef9605694d57e81",
+    "url": "/fact-seagull/static/css/main.8b00d115.chunk.css"
+  },
+  {
+    "revision": "06e1ef3955d47690018d",
+    "url": "/fact-seagull/static/js/2.c919ac89.chunk.js"
+  },
+  {
+    "revision": "5a898ef9605694d57e81",
+    "url": "/fact-seagull/static/js/main.18314a5c.chunk.js"
+  },
+  {
+    "revision": "9bda4411373aa97d35f5",
+    "url": "/fact-seagull/static/js/runtime-main.ecbef938.js"
+  },
+  {
+    "revision": "9ff83de09b1009aed03bf009e6f475eb",
+    "url": "/fact-seagull/static/media/Apple-Kid.9ff83de0.ttf"
+  },
+  {
+    "revision": "f0a90afdded617966023fe2d99a183e9",
+    "url": "/fact-seagull/static/media/Apple-Kid.f0a90afd.woff"
+  },
+  {
+    "revision": "93c9ba4911c241d6d187daccec936e21",
+    "url": "/fact-seagull/static/media/sad_loop.93c9ba49.mp3"
+  }
+]);
