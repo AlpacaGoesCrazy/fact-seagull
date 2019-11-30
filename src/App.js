7f661170c2592bbb56206b6d4a80111628edccfd
AlpacaGoesCrazy <vancho.karamba@gmail.com>
2019-11-30 18:29:21 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
+import img from './media/animation.gif'
+import audio from './media/sad_loop.mp3'
+import darkness from './data/darkness.json'
+import facts from './data/facts.json'
+
+function getRandomFact() {
+  const shouldDieInside = Math.random() < 0.3
+  const factArray = shouldDieInside ? darkness : facts
+  const i = Math.floor(Math.random() * factArray.length)
+  return factArray[i]
+}
+
+function getViewLength(fact) {
+  const averageReadSpeed = 200 
+  const wordNumber = fact.split(" ").length
+  return Math.ceil(wordNumber / averageReadSpeed * 60 * 1.2 * 1000)
+}
 
 function App() {
+  const [fact, setFact] = useState(() => getRandomFact())
+  const interval = useRef()
+
+  interval.current = setTimeout(() => {
+    setFact(getRandomFact())
+  }, getViewLength(fact))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Fact Seagull says:</p>
+      <img src={img} alt="seagull"/>
+      <p className="Fact">{fact}</p>
+      <audio autoPlay loop>
+        <source src={audio} type="audio/mpeg"/>
+      </audio> 
     </div>
   );
 }
